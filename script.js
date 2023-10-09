6bc1886fa091096bc4326e276e9adcc0fd2cfaad
federicopignatelli <federicopignatelli@icloud.com>
2023-10-09 16:14:09 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+class user {
+    constructor(_firstName, _lastName, _age, _location) {
+        this.firstName = _firstName
+        this.lastname = _lastName
+        this.age = _age
+        this.location = _location
+    }
+}
